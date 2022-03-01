770741ff5c0dae71247ca26e8b1e625a83cb7ead
memogamer9927 <76068640+memogamer9927@users.noreply.github.com>
2022-03-01 12:46:47 -0600

    Update NavBar.js

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,7 +1,19 @@
+import {Row, Col} from 'react-bootstrap';
+import '../App.css';
+
 const NavBar = () => {
     return (
-        <div>
-        </div>
+        <Row id="contenedor-menu">
+            <Col sm id='opcion-select'>
+                Candidatos en Proceso
+            </Col>
+            <Col sm id ='opcion'>
+                Candidatos Contratados
+            </Col>
+            <Col sm id ='opcion'>
+               Vacantes
+            </Col>
+        </Row>
     );
 }
 
